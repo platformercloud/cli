4f5236f0faea2a7f925241bed85b421558ecb52b
Chathuranga Dassanayake <chathura2ihl@gmail.com>
2020-12-14 22:58:17 +0530

    fix .har output directory

--- src/modules/util/fetch.ts
+++ src/modules/util/fetch.ts
@@ -1,7 +1,9 @@
 import * as fs from 'fs';
 import nodeFetch from 'node-fetch';
-import { withHar, createHarLog } from 'node-fetch-har';
+import { createHarLog, withHar } from 'node-fetch-har';
 import * as path from 'path';
+import { homedir } from 'os';
+const homeDir = homedir();
 
 const har: any[] = [];
 
@@ -9,10 +11,7 @@ export const fetch = withHar(nodeFetch, {
   onHarEntry: (entry: any) => har.push(entry),
 }) as typeof nodeFetch;
 
-export function writeHAR(
-  dir = `/home/kcd/logs/`,
-  fileName = `har-${new Date().toISOString()}.har`
-) {
-  const filePath = path.join(dir, fileName);
-  fs.writeFileSync(filePath, JSON.stringify(createHarLog(har), null, 2));
+export function writeHAR(fileName = `har-${new Date().toISOString()}.har`) {
+  // const filePath = path.join(homeDir, 'logs', fileName);
+  // fs.writeFileSync(filePath, JSON.stringify(createHarLog(har), null, 2));
 }
