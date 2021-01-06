aee503e17c0c5e6ceabcbd73aae7a1aa81d55b0c
Binura Gunasekara <binura.g@gmail.com>
2021-01-06 18:40:08 +1100

    Fixed tslint issues

--- test/commands/contex.test.ts
+++ test/commands/contex.test.ts
@@ -1,10 +1,10 @@
 import { expect, test } from '@oclif/test';
 
-describe('context:list', () => {
+describe('command: context', () => {
   test
     .stdout()
     .command(['context:list'])
     .it('displays a list of contexts', (ctx) => {
-      expect(ctx.stdout).to.contain('default');
+      expect(ctx.stdout).to.not.be.empty;
     });
 });
