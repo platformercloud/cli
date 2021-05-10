f534211ed1598dce94c260bd8e958394535cd115
ebonynon <sithumsandeepap@gmail.com>
2021-05-10 08:51:40 +0530

    Rm unused validate

--- src/modules/util/rudder_validations.ts
+++ src/modules/util/rudder_validations.ts
@@ -18,36 +18,11 @@ function createHostNameValidator(length: number, msg: string) {
   };
 }
 
-export const validateNamespace = createHostNameValidator(
-  50,
-  'Must be a valid namespace'
-);
 export const validateContainerName = createHostNameValidator(
   50,
   'Must be a valid container name'
 );
-export const validateEnvName = createHostNameValidator(
-  50,
-  'Must be a valid name'
-);
-export const validateK8name50 = createHostNameValidator(
-  50,
-  'Must be a valid kubernetes name'
-);
-export const validateK8name20 = createHostNameValidator(
-  20,
-  'Must be a valid kubernetes name'
-);
-export const validateRudderName50 = createHostNameValidator(
-  50,
-  'Must be a valid name'
-);
 export const validateAppName50 = createHostNameValidator(
-  50,
-  'Must be a valid name'
-);
-
-export const validateVolumeName = createHostNameValidator(
   50,
   'Must be a valid name'
 );
